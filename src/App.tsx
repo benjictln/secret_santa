b0ea2d599749e2634d69aa4b875f10551d42eb0b
Benjamin Castellan <benjamincastellan@gmail.com>
2022-12-03 21:13:06 +0400

    displaying final names

--- src/App.tsx
+++ src/App.tsx
@@ -1,96 +1,27 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
-import DeletableChips from "./components/DeletableChips";
-import {Box, InputAdornment, Snackbar, TextField} from "@mui/material";
-import {AccountCircle, AddCircleRounded} from "@mui/icons-material";
+import {RegisterPersonPage} from "./components/RegisterPersonPage";
+import FinishSendEmail from "./components/FinishSendEmail";
+import {Person} from "./Person";
 
-class Person {
-    name: string;
-    email: string;
 
-    constructor(theName: string, theEmail: string) {
-        this.name = theName;
-        this.email = theEmail;
-    }
-}
+const STAGE_REGISTER_PERSONS = 0;
+const STAGE_SENDING_EMAIL = 1;
 
 function App() {
 
-    const [currentName, setCurrentName] = useState("")
-    const [currentEmail, setCurrentEmail] = useState("")
-    const [persons, setPersons] = useState([
-        new Person("Ben", "ben@gmail.com"),
-        new Person("tom1", "tom@gmail.com"),
-        new Person("tom2", "tom@gmail.com"),
-        new Person("tom3", "tom@gmail.com"),
-        new Person("tom4", "tom@gmail.com"),
-    ]);
-    const [openError, setOpenError] = useState(false);
+    const [persons, setPersons] = useState<Person[]>([]);
 
-    const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
-        if (reason === 'clickaway') {
-            return;
-        }
+    const prepareSendingEmail = (persons: Person[]) => {
+        setPersons(persons)
+        setStage(STAGE_SENDING_EMAIL)
 
-        setOpenError(false);
-    };
-
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-
-
-                <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-                    <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-
-                    <TextField label="Name" variant="standard" value={currentName}
-                               onChange={(event) => setCurrentName(event.target.value)}/>
-
-                    <TextField label="Email" variant="standard" value={currentEmail}
-                               onChange={(event) => setCurrentEmail(event.target.value)}/>
-                    <AddCircleRounded onClick={() => {
-                        if (currentName.trim() === "") {
-                            setOpenError(true)
-                            return
-                        }
-                        const expression: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-                        if (!expression.test(currentEmail)) {
-                            setOpenError(true)
-                            return
-                        }
-
-                        setPersons(persons.concat(new Person(currentName, currentEmail)))
-                        setCurrentName("")
-                        setCurrentEmail("")
-
-                            }
-                    }/>
-                </Box>
-
-
-
-                {persons.map( (person, index) => (
-                    <DeletableChips name={person.name} handleDelete={ () => {
-
-                        setPersons(persons.filter((_, index2) =>
-                            index2 !== index
-                        ));
-
-                    }}/>
-                ))}
-            </header>
-
-            <Snackbar
-                open={openError}
-                autoHideDuration={6000}
-                onClose={handleClose}
-                message="Wrong inputs"
-                // action={action}
-            />
-        </div>
-    );
+    }
+    const [stage, setStage] = useState(STAGE_REGISTER_PERSONS)
+    if (stage === STAGE_REGISTER_PERSONS) {
+        return (<RegisterPersonPage prepareSendingEmail={prepareSendingEmail}/>)
+    }
+    return (<FinishSendEmail persons={persons}/>)
 }
 
 export default App;
